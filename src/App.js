a34f77d249323741aaafb09a07a0a5cf7496cea4
cpscott1 <thacam18gmail.com>
2020-09-12 10:12:12 -0500

    added header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 import './App.css';
+import logo from './assets/logo.png';
+import './header.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button>
+          <FontAwesomeIcon className="menu-icon" icon={faBars} />
+        </button>
+        <img src={logo} alt="logo" className="logo" />
       </header>
     </div>
   );
