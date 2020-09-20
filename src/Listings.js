a15ca9b029dc8710181b4ce5c4a0ef113893a6ce
cpscott1 <thacam18gmail.com>
2020-09-20 10:51:43 -0500

    working more on listing section

--- src/Listings.js
+++ src/Listings.js
@@ -4,35 +4,38 @@ import './listing.scss'
 
 export default () => (
   <section className="listing-section">
-    <form>
-      <div>
+    <form className="listing">
+      <div className="listing-form">
         <label htmlFor="lookingFor">LOOKING FOR</label>
         <input
           name="lookingFor"
           placeholder="Property Type"
         />
       </div>
-      <div>
+      <div className="listing-form">
         <label htmlFor="location">LOCATION</label>
         <input
           name="location"
           placeholder="All Cities"
         />
       </div>
-      <div>
+      <div className="listing-form">
         <label htmlFor="propertySize">PROPERTY SIZE</label>
         <input
           name="propertySize"
           placeholder="Bedrooms"
         />
       </div>
-      <div>
+      <div className="listing-form">
         <label htmlFor="yourBudget">YOUR BUDGET</label>
         <input
           name="yourBudget"
           placeholder="Max. Price"
         />
       </div>
+      <button className="listing-search">Search</button>
     </form>
+    <h1>Discover Our Featured <br />Listings</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur <br /> adipisic edut</p>
   </section>
 );
