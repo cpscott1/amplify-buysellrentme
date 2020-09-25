bd0ad3cdd54c7e00a4ee5a1a95beb0df4a7f8bbb
cpscott1 <thacam18gmail.com>
2020-09-25 17:30:01 -0500

    added more dummy data

--- src/data/residentialData.js
+++ src/data/residentialData.js
@@ -4,5 +4,20 @@ export const residentialData = [
     properties: '17',
     propertyType: 'Apartment',
     image: require('../assets/apartment.png')
+  },
+  {
+    properties: '17',
+    propertyType: 'Studio',
+    image: require('../assets/studio.png')
+  },
+  {
+    properties: '17',
+    propertyType: 'Single Family Home',
+    image: require('../assets/single-family.png')
+  },
+  {
+    properties: '17',
+    propertyType: 'Villa',
+    image: require('../assets/villa.png')
   }
 ]
