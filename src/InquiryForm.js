f034cb92803fa87f1b624409c2a1832040e245cb
cpscott1 <thacam18gmail.com>
2020-09-26 02:27:09 -0500

    added more to form

--- src/InquiryForm.js
+++ src/InquiryForm.js
@@ -16,6 +16,27 @@ export default () => (
         </select>
         <input type='text' name='first-name' placeholder='First Name' />
         <input type='text' name='last-name' placeholder='Last Name' />
+        <input type='email' name='email' placeholder='email' />
+      </div>
+      <div>
+        <label>Location</label>
+        <select name="Select">
+          <option>Select</option>
+        </select>
+        <input type='text' name="zip-code" placeholder='Zip Code' />
+      </div>
+      <div>
+        <label>Property</label>
+        <select name="Select Type">
+          <option>Select Type</option>
+        </select>
+        <input type='text' name="max-price" placeholder='Max Price' />
+        <input type="text" name="min-size" placeholder="Minimum size" />
+        <input type="text" name="bed-number" placeholder="No of Beds" />
+        <input type="text" name="bath-number" placeholder="No of Baths" />
+      </div>
+      <div>
+        <span>GDPR Agreement</span>
       </div>
     </form>
   </section>
