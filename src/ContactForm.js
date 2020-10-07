2c6f663a7c133a876d492030f33933f51964270e
cpscott1 <thacam18gmail.com>
2020-10-07 02:51:18 -0500

    fixed styling issues on form

--- src/ContactForm.js
+++ src/ContactForm.js
@@ -3,16 +3,26 @@ import './contact.scss'
 
 export default () => (
   <section className="contact-section">
-    <form className="contact-form">
-      <span>If you are unable to locate the answers on our FAQ page or just have a general question, please use the form below to get in contact with us.</span>
-      <div className="contact">
-        <label>First Name</label>
-        <input type="text" name="first-name" placeholder="first name" />
-        <label>Last Name</label>
-        <input type="text" name="last-name" placeholder="last name" />
-        <label>Email</label>
-        <input type="email" name="email" placeholder="email" />
-      </div>
-    </form>
+    <div className="contact">
+      <form className="contact-form">
+        <span className="contact-header">If you are unable to locate the answers on our FAQ <br />page or just have a general question, please use the <br/> form below to get in contact with us.</span>
+          <div className="input-container">
+            <label className="contact-label">First Name</label>
+            <input className="contact-input" type="text" name="first-name" placeholder="first name" />
+          </div>
+          <div className="input-container">
+            <label className="contact-label">Last Name</label>
+            <input className="contact-input" type="text" name="last-name" placeholder="last name" />
+          </div>
+          <div className="input-container">
+            <label className="contact-label">Email</label>
+            <input className="contact-input" type="email" name="email" placeholder="email" />
+          </div>
+          <div className="input-container">
+            <label className="contact-label">Message</label>
+            <textarea />
+          </div>
+      </form>
+    </div>
   </section>
 )
