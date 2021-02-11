c6e870c34f50df5b2ca80a785c475c1426c20d0f
cpscott1 <thacam18gmail.com>
2021-02-10 23:42:52 -0600

    new changes

--- src/Property.js
+++ src/Property.js
@@ -6,6 +6,28 @@ export default ({ propertyData }) => (
     <div className="property-main">
       <h1 className="property-header">Check Our Selection of Finest Properties</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+      <div className="property-col-1">
+        <div className="property-image-1">
+          <h3>Featured</h3>
+        </div>
+        <div className="property-image-1">
+          <h3>Featured</h3>
+        </div>
+        <div className="property-image-1">
+          <h3>Featured</h3>
+        </div>
+      </div>
+      <div className="property-col-2">
+        <div className="property-image-1">
+          <h3>Featured</h3>
+        </div>
+        <div className="property-image-1">
+          <h3>Featured</h3>
+        </div>
+        <div className="property-image-1">
+          <h3>Featured</h3>
+        </div>
+      </div>
       {propertyData.map((property, index) => {
         return (
           <div key={index}>
